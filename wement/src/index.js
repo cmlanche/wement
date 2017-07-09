8572a31d83431e94c8831aa4c6b6837a9de127b4
chengming <chengming@testin.cn>
2017-07-10 00:01:25 +0800

    完善评论界面

--- wement/src/index.js
+++ wement/src/index.js
@@ -25,6 +25,8 @@ document.getElementById("wm-add-comment-btn").onclick = function (e) {
     wm_addComment();
 };
 
+wm_autofit_textarea(document.getElementById("wm-content-textarea"));
+
 /**
  * 请求授权
  */
@@ -64,9 +66,9 @@ function wm_setWementInfo(data) {
     if(wm_wement.code === -1){
         // unauth
         wm_requestAuth();
-    }else{
+    }else if(wm_wement.code == 0){
         wm_log("success");
-        document.getElementById("wm-user-info").innerText = wm_wement.data.map.user.map.nickname;
+        document.getElementById("wm-user-headimage").src = wm_wement.user.headimage;
     }
 }
 
@@ -75,7 +77,7 @@ function wm_setWementInfo(data) {
  */
 function wm_addComment() {
     if(wm_wement){
-        let content = document.getElementById("wm-content-input").value;
+        let content = document.getElementById("wm-content-textarea").value;
         http.post("/comment/add", {
             "websiteId": wm_getWebsiteId(),
             "domain": document.location.host,
@@ -97,6 +99,13 @@ function wm_addComment() {
 }
 
 /**
+ * 删除一条评论
+ */
+function wm_delComment(e, id) {
+    alert("ddd");
+}
+
+/**
  * 获取当前页面评论列表
  */
 function wm_getComments() {
@@ -107,12 +116,51 @@ function wm_getComments() {
         }).then(res=>{
             let json = JSON.parse(res);
             if(json.code == 0){
-                let dom_comment = document.getElementById("wm_comments");
-                for(let comment of json.data.map.comments.list){
-                    wm_log(comment.map);
+                let dom_comment = document.getElementById("wm_comment");
+                for(let comment of json.comments){
+                    wm_log(comment);
+                    let content = comment.content;
                     let dom_comment_item = document.createElement("div");
                     dom_comment_item.className = "wm-comment-item";
-                    dom_comment_item.innerHTML = `<span>${comment.map.content}</span>`;
+
+                    // 添加头像
+                    let dom_comment_user = document.createElement("div");
+                    dom_comment_user.className = "wm-user";
+                    let dom_comment_userimage = document.createElement("img");
+                    dom_comment_userimage.className = "wm-user-headimage";
+                    dom_comment_userimage.src = "wm_default_headimage.jpg";
+                    dom_comment_user.appendChild(dom_comment_userimage);
+
+                    // 添加评论内容
+                    let dom_comment_content = document.createElement("div");
+                    dom_comment_content.className = "wm-comment-content";
+
+                    let feedback;
+                    if(wm_wement.user._id != comment.wmUserid){
+                        feedback = `<a>回复</a>
+                                    <a>赞</a>
+                                    <a>举报</a>`;
+                    }else{
+                        feedback = `<a href="javascript:void(0)" onclick="wm_getWementInfo();">删除</a>`;
+                    }
+                    dom_comment_content.innerHTML =
+                        `<div class="wm-user-name">
+                             cmlanche
+                        </div>
+                        <div class="wm-comment-user-content">
+                            <p>${content}</p>
+                            <div class="wm-comment-user-content-bottom">
+                                <time>2年前<time/>
+                                <span>
+                                    ${feedback}
+                                </span>
+                                <span class="wm-comment-user-content-bottom-sup">5赞</span>
+                            </div>
+                        </div>`;
+
+                    dom_comment_item.appendChild(dom_comment_user);
+                    dom_comment_item.appendChild(dom_comment_content);
+
                     dom_comment.appendChild(dom_comment_item);
                 }
                 wm_log("get comments success");
@@ -124,5 +172,68 @@ function wm_getComments() {
 }
 
 function wm_getWebsiteId() {
-    return wm_wement.data.map.website.map._id;
+    return wm_wement.website._id;
 }
+
+function wm_autofit_textarea(elem, extra, maxHeight) {
+    extra = extra || 0;
+    var isFirefox = !!document.getBoxObjectFor || 'mozInnerScreenX' in window,
+        isOpera = !!window.opera && !!window.opera.toString().indexOf('Opera'),
+        addEvent = function (type, callback) {
+            elem.addEventListener ?
+                elem.addEventListener(type, callback, false) :
+                elem.attachEvent('on' + type, callback);
+        },
+        getStyle = elem.currentStyle ? function (name) {
+                var val = elem.currentStyle[name];
+
+                if (name === 'height' && val.search(/px/i) !== 1) {
+                    var rect = elem.getBoundingClientRect();
+                    return rect.bottom - rect.top -
+                        parseFloat(getStyle('paddingTop')) -
+                        parseFloat(getStyle('paddingBottom')) + 'px';
+                };
+
+                return val;
+            } : function (name) {
+                return getComputedStyle(elem, null)[name];
+            },
+        minHeight = parseFloat(getStyle('height'));
+
+    elem.style.resize = 'none';
+
+    var change = function () {
+        var scrollTop, height,
+            padding = 0,
+            style = elem.style;
+
+        if (elem._length === elem.value.length) return;
+        elem._length = elem.value.length;
+
+        if (!isFirefox && !isOpera) {
+            padding = parseInt(getStyle('paddingTop')) + parseInt(getStyle('paddingBottom'));
+        };
+        scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+
+        elem.style.height = minHeight + 'px';
+        if (elem.scrollHeight > minHeight) {
+            if (maxHeight && elem.scrollHeight > maxHeight) {
+                height = maxHeight - padding;
+                style.overflowY = 'auto';
+            } else {
+                height = elem.scrollHeight - padding;
+                style.overflowY = 'hidden';
+            };
+            style.height = height + extra + 'px';
+            scrollTop += parseInt(style.height) - elem.currHeight;
+            document.body.scrollTop = scrollTop;
+            document.documentElement.scrollTop = scrollTop;
+            elem.currHeight = parseInt(style.height);
+        };
+    };
+
+    addEvent('propertychange', change);
+    addEvent('input', change);
+    addEvent('focus', change);
+    change();
+};
